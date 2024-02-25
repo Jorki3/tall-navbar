97cd8d280a0727024ff57775a7d8e26ec221207a
Eduardo Trevizo <50352230+Jorki3@users.noreply.github.com>
2024-02-25 01:51:01 -0600

    adjust colors

--- resources/views/components/a-navbar.blade.php
+++ resources/views/components/a-navbar.blade.php
@@ -1,3 +1,4 @@
-<a href="{{ $href }}" class="py-2 text-xl text-center md:px-4 md:py-7 hover:bg-zinc-800 hover:text-zinc-300">
+<a href="{{ $href }}"
+    class="py-2 text-xl text-center md:px-4 md:py-7 text-zinc-300 dark:text-zinc-800 dark:hover:text-zinc-300 hover:bg-zinc-300 dark:hover:bg-zinc-800 hover:text-zinc-800 ">
     {{ $text }}
 </a>
